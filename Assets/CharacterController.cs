1d6bb32ef2990cfded7c259abaa32c57947b208d
sabrinagelbart <sabrina.gelbart@gmail.com>
2017-04-02 22:46:20 -0500

    third person controller working but animals still hover

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; //can make changes in the editor!
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() //called before doing any physics calculations
+	{
+		float moveHoriztonal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHoriztonal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+	}
+
+}

2b52b02896072dfbd72cb75c242e05073b51ea2e
Zo <ineffablezoe@gmail.com>
2017-04-07 20:35:24 -0500

    tyring to fix farmer

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -10,9 +10,17 @@ public class PathFollower : MonoBehaviour {
 	public static float speed = 5.0f;	//holds speed of obj
 	public float reachDist = 1.0f;
 	public int currentPoint = 0;		//tells index within path
-	public int[] turnPoints;
+	public int[] leftTurns;
+	public int[] rightTurns;
 
 	public AnimationClip idle;
+	public AnimationClip walk;
+
+	void Start ()
+	{
+		walk.legacy = true;
+		GetComponent <Animation> ().Play ("walk");
+	}
 
 	void Update () {
 		float dist = Vector3.Distance (path [currentPoint].position, transform.position);
@@ -22,9 +30,13 @@ public class PathFollower : MonoBehaviour {
 		if (dist <= reachDist)
 		{
 			currentPoint++;
-			if (turnPoints.Contains (currentPoint))
+			if (leftTurns.Contains(currentPoint))
 			{
 				transform.Rotate(0,-90,0);
+			}
+			if (rightTurns.Contains(currentPoint))
+			{
+				transform.Rotate (0,90,0);
 			}
 		}
 

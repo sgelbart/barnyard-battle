3e23db17ecf4654c26f776e171e84fb92d681751
Ilana Rozzell <akame13579@gmail.com>
2017-04-07 20:22:48 -0500

    Carrots Placed

---
+++ Assets/PickUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	public float targetTime = 60.0f;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: 22/ " + count.ToString ();
+		if (count >= 12) {
+			winText.text = " ";
+		}
+	}
+}

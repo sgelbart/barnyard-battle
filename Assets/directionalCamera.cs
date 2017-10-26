b2c6f9d8bbc97d36d6f0699855421ff4f565c777
Prussia <akame13579@gmail.com>
2017-10-26 18:11:24 -0500

    Camera Movement Working

---
+++ Assets/directionalCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public GameObject player;
+	public float thrust = 2.0f;
+
+	void FixedUpdate()
+	{
+		player.transform.position += this.transform.forward * thrust * Time.deltaTime;
+	}
+
+}
